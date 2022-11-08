8e3640573d08deefaa6512f070f3ee2a1d50c065
Olesia Libra <98489034+Solymar1388@users.noreply.github.com>
2022-11-08 16:17:51 +0200

    fixed bugs

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let text = `It's time to relax...`;
 let i = 0;
 let speed = 200;
@@ -29,35 +28,3 @@ cards.forEach((card) => {
     });
   };
 });
-=======
-let text = `It's time to relax...`;
-let i = 0;
-let speed = 200;
-
-function type() {
-  if (i < text.length) {
-    document.querySelector('#title').textContent += text.charAt(i);
-    i++;
-    setTimeout(type, speed);
-  }
-}
-
-type();
-
-const cards = document.querySelectorAll('.card');
-
-cards.forEach((card) => {
-  card.addEventListener('mouseover', () => {
-    removeFocus();
-    card.classList.add('rotated');
-  });
-
-  removeFocus = () => {
-    cards.forEach((card) => {
-      card.addEventListener('mouseleave', () => {
-        card.classList.remove('rotated');
-      });
-    });
-  };
-});
->>>>>>> 84d2485a61b07f60cc3ea50f48976808797f4a23
